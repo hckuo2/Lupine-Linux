43dc85333ffe0cf0982b2307359ec230ac398874
Hsuan-Chi Kuo <hckuo@ibm.com>
2019-07-22 15:08:09 -0400

    update script

---
+++ scripts/build-with-configs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+# This file takes config files and build a kernel base on the config
+# The kernel built would be placed into a directory
+
+KERNELBUILD="$(pwd)/kernelbuild"
+LINUX="linux"
+CONFIGS=$@
+DIRNAME=""
+for config in $CONFIGS; do
+    filename=$(basename $config)
+    DIRNAME="$DIRNAME++${filename%.*}"
+done
+DIRNAME=${DIRNAME:2}
+BUILDDIR=$KERNELBUILD/$DIRNAME
+
+no() {
+    while true; do
+        echo "n"
+    done
+}
+mkdir -p $BUILDDIR
+rm $LINUX/.config
+for config in $CONFIGS; do
+    cat $config >> $LINUX/.config
+done
+echo "Building kernel"
+no | make -C linux oldconfig
+make build-linux && \
+    pushd $LINUX && \
+    cp vmlinux $BUILDDIR && \
+    INSTALL_PATH=$BUILDDIR make install;
