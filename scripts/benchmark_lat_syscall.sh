3ddb3228c1421ccdfe8cd7d2a55b6f76dfdf6a94
Dan Williams <djwillia@us.ibm.com>
2019-08-20 11:44:34 -0400

    more configs and syscall lat experiment

---
+++ scripts/benchmark_lat_syscall.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+source scripts/run-helper.sh
+rep=1000000
+itr=20
+types="null read write"
+osv=~/osv
+
+mean() {
+    awk '{sum+=$1}END{print sum/NR}'
+}
+
+stat() {
+    awk '{x+=$0;y+=$0^2}END{print x/NR, sqrt(y/NR-(x/NR)^2)}'
+}
+
+run_test() {
+    SYS=$1
+    KERNEL=$2
+    BIN=$3
+    for t in $types; do 
+        echo -n "$KERNEL $BIN $t $itr $rep "
+        for i in `seq $itr`; do
+            if [ "x$SYS" == "xvm" ]; then
+                firectl --firecracker-binary=$(pwd)/firecracker \
+                        --kernel $KERNEL \
+                        --tap-device=tap100/AA:FC:00:00:00:01 \
+                        --root-drive=lat_syscall.ext2 \
+                        --kernel-opts="console=ttyS0 noapic  panic=-1 pci=off nomodules rw init=$BIN -N $rep $t"
+            elif [ "x$SYS" == "xosv" ]; then
+                sudo $osv/scripts/firecracker.py -e "/lat_syscall -N $rep $t"
+            elif [ "x$SYS" == "xhermitux" ]; then
+                docker run -v ~/hermitux:/hermitux --rm --privileged -it olivierpierre/hermitux bash -c "/hermitux/apps/lmbench3/lat_syscall/run-test2.sh $rep $t"
+            elif [ "x$SYS" == "xhost" ]; then
+                docker run --rm -it lat_syscall -N $rep $t
+            fi
+        done 2>&1 | grep "res:" | cut -d: -f2 | stat
+    done
+}
+
+run_test host host /lat_syscall
+# run_test hermitux hermitux /lat_syscall
+# run_test osv osv /lat_syscall
+# run_test vm $(pwd)/kernelbuild/microvm/vmlinux /lat_syscall
+# run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /lat_syscall
+# run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /trusted/lat_syscall
+
+
+
+#run_test vm $(pwd)/kernelbuild/microvm-paravirt+kml/vmlinux /lat_syscall
+#run_test vm $(pwd)/kernelbuild/lupine+kml+mmio+O2/vmlinux /lat_syscall
+# run_test vm $(pwd)/kernelbuild/lupine+kml+mmio/vmlinux /trusted/lat_syscall
