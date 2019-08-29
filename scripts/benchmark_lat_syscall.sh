ca43bcfa1c24fbb2d23a69c71b5c61016d67e1ae
Dan Williams <djwillia@us.ibm.com>
2019-08-29 14:44:50 -0400

    recompute lat_syscall

--- scripts/benchmark_lat_syscall.sh
+++ scripts/benchmark_lat_syscall.sh
@@ -16,9 +16,11 @@ stat() {
 run_test() {
     SYS=$1
     KERNEL=$2
-    BIN=$3
+    shift
+    shift
+    BIN=$@
     for t in $types; do 
-        echo -n "$KERNEL $BIN $t $itr $rep "
+        echo -n "$KERNEL ${BIN/ /,} $t $itr $rep "
         for i in `seq $itr`; do
             if [ "x$SYS" == "xvm" ]; then
                 firectl --firecracker-binary=$(pwd)/firecracker \
@@ -31,18 +33,18 @@ run_test() {
             elif [ "x$SYS" == "xhermitux" ]; then
                 docker run -v ~/hermitux:/hermitux --rm --privileged -it olivierpierre/hermitux bash -c "/hermitux/apps/lmbench3/lat_syscall/run-test2.sh $rep $t"
             elif [ "x$SYS" == "xhost" ]; then
-                docker run --rm -it lat_syscall -N $rep $t
+                docker run --rm -it lat_syscall $BIN -N $rep $t
             fi
         done 2>&1 | grep "res:" | cut -d: -f2 | stat
     done
 }
 
-#run_test host host /lat_syscall
-# run_test hermitux hermitux /lat_syscall
-# run_test osv osv /lat_syscall
-# run_test vm $(pwd)/kernelbuild/microvm/vmlinux /lat_syscall
-run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /lat_syscall
-run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /trusted/lat_syscall
+run_test host host /libc.so /lat_syscall
+run_test hermitux hermitux /lat_syscall
+run_test osv osv /lat_syscall
+run_test vm $(pwd)/kernelbuild/microvm/vmlinux /libc.so /lat_syscall
+run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /libc.so /lat_syscall
+run_test vm $(pwd)/kernelbuild/lupine-djw-kml/vmlinux /trusted/libc.so /trusted/lat_syscall
 
 
 
