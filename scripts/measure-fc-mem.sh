d86a706a7e25c90e2cdbf5999d379be3304cf585
Hsuan-Chi Kuo <hckuo@ibm.com>
2019-08-31 16:07:14 -0400

    add timeout

--- scripts/measure-fc-mem.sh
+++ scripts/measure-fc-mem.sh
@@ -9,7 +9,7 @@ KERNEL_OPTS+="ipv6.disable=1 init=/hello"
 [ -f hello-world.ext2 ] || ./scripts/image2rootfs.sh hello-world latest ext2
 
 left=0
-right=512
+right=1024
 mem=$right
 while [ $left -le $right ]; do
     mem=$(((left+right)/ 2))
@@ -18,13 +18,17 @@ while [ $left -le $right ]; do
         --kernel $KERNEL \
         --root-drive=hello-world.ext2  \
         --kernel-opts="$KERNEL_OPTS" \
-        --memory=$mem 2> $VMM_STDERR > $VMM_STDOUT
+        --memory=$mem 2> $VMM_STDERR > $VMM_STDOUT &;
+
+    # if we do not see the desired output from the guest after this timeout we
+    # consider it not working.
+    sleep 5
+    pkill firecracker
     if grep -q "Hello from Docker" $VMM_STDOUT; then
         right=$(($mem - 1))
     else
         left=$(($mem + 1))
     fi
-    sleep 1
     rm $VMM_STDOUT $VMM_STDERR
 done
-echo $left $mem $right
+$mem
