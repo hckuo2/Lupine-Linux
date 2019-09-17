df1b10414e0b4f3edf381764081d395f01e44716
Hsuan-Chi Kuo <hckuo@ibm.com>
2019-09-17 17:08:51 -0400

    add redis measure for vm

--- scripts/guest_start.sh
+++ scripts/guest_start.sh
@@ -3,19 +3,16 @@ export PATH="/usr/local/bin:$PATH"
 sh guest_net.sh
 mkdir -p /trusted
 if which nginx; then
-    echo "daemon off;" >> /etc/nginx/nginx.conf
     cp `which nginx` /trusted
 fi
 if which redis-server; then
     cp `which redis-server` /trusted
 fi
-ls -l /usr/local/bin
-ls -l /trusted
 echo $1
-if [[ $1 == "/trusted/"* ]]; then
+if [ x"$1" == "x/trusted/"* ]; then
     echo "KML mode is really on!"
-    /trusted/libc.so $@
+    /trusted/libc.so $@ -g 'daemon off;'
 else
     echo "KML mode is really off!"
-    $@
+    $@ -g 'daemon off;'
 fi
