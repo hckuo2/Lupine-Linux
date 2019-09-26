68cb5ab470c311817759266393a0118d2d07d10c
Dan Williams <djwillia@us.ibm.com>
2019-09-26 15:43:26 -0400

    postgres config

--- scripts/guest_start.sh
+++ scripts/guest_start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export PATH="/usr/local/bin:$PATH"
-#sh guest_net.sh
 mkdir -p /trusted
 
 echo "APP START"
@@ -12,11 +11,13 @@ if which php; then
 fi
 
 if which mysql; then
+    sh guest_net.sh
     echo ========NOKML=========
     MYSQL_ALLOW_EMPTY_PASSWORD=yes /entrypoint.sh mysqld
 fi
 
 if which postgres; then
+    sh guest_net.sh
     echo ========NOKML=========
     export POSTGRES_PASSWORD=mysecretpassword
     export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -25,19 +26,21 @@ if which postgres; then
     export PG_VERSION=11.5
     export PG_SHA256=7fdf23060bfc715144cbf2696cf05b0fa284ad3eb21f0c378591c6bca99ad180
     export PGDATA=/var/lib/postgresql/data
+    ./guest_load_entropy 1000
 
     echo "mysecretpassword" > /pw
     sed -i s/"--pwfile=<(echo \"\\\$POSTGRES_PASSWORD\")"/"--pwfile=\/pw"/ docker-entrypoint.sh
-    #    cat docker-entrypoint.sh | sed s/"--pwfile=<(echo \"\\\$POSTGRES_PASSWORD\")"/"--pwfile=\/tmp\/pw"/ | grep pwfile
-    #/docker-entrypoint.sh postgres
+    /docker-entrypoint.sh postgres &
 fi
 
 if which mongo; then
+    sh guest_net.sh
     echo ========NOKML=========
     /usr/local/bin/docker-entrypoint.sh mongod   
 fi
 
 if which nginx; then
+    sh guest_net.sh
     cp `which nginx` /trusted
     if echo $@ | grep trusted - > /dev/null; then
         echo ========KML=========
@@ -48,6 +51,7 @@ if which nginx; then
     fi
 fi
 if which redis-server; then
+    sh guest_net.sh
     cp `which redis-server` /trusted
     if echo $@ | grep trusted - > /dev/null; then
         echo ========KML=========
