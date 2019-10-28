45c2bf39ea5ddffa967c25ec331dbdede8d62179
Ricardo Koller <ricarkol@gmail.com>
2019-10-28 11:41:03 -0400

    mariadb config and scripts

--- scripts/guest_start.sh
+++ scripts/guest_start.sh
@@ -6,6 +6,32 @@ ulimit -n 65535
 
 echo "APP START"
 
+if which mysqld; then
+    sh guest_net.sh
+    export MYSQL_ALLOW_EMPTY_PASSWORD=1
+    /load_entropy 1000
+    rm -f /var/lib/mysql/aria_log_control /ibdata1
+    /docker-entrypoint.sh mysqld
+    exit
+fi
+
+if which postgres; then
+    sh guest_net.sh
+    echo ========NOKML=========
+    export MYSQL_ROOT_PASSWORD=mysecretpassword
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+    export LANG=en_US.utf8
+    export PG_MAJOR=11
+    export PG_VERSION=11.5
+    export PG_SHA256=7fdf23060bfc715144cbf2696cf05b0fa284ad3eb21f0c378591c6bca99ad180
+    export PGDATA=/var/lib/postgresql/data
+    ./guest_load_entropy 1000
+
+    echo "mysecretpassword" > /pw
+    sed -i s/"--pwfile=<(echo \"\\\$POSTGRES_PASSWORD\")"/"--pwfile=\/pw"/ docker-entrypoint.sh
+    /docker-entrypoint.sh postgres &
+    exit
+fi
 
 if [ -f /usr/local/go/bin/go ]; then
     echo "package main" >> test.go
